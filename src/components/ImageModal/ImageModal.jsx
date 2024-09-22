fee0fc190301b25ed8b87c5ecb2ba8d2e32867b1
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-09-22 15:29:18 +0300

    add

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
-
+import s from "./ImageModal.module.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
 const customStyles = {
   content: {
     top: "50%",
@@ -8,6 +9,19 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    overflow: "hidden",
+    borderRadius: "5px",
+    padding: "20px",
+    background: "black",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "85vh",
+    width: "90vw",
+    flexWrap: "nowrap",
+    boxShadow: "0 5px 10px rgba(0, 0, 0, 0.2)",
+    zIndex: 1000,
+    transition: "opacity 0.3s ease-in-out",
   },
 };
 
@@ -20,12 +34,27 @@ const ImageModal = ({ modalIsOpen, closeModal, image }) => {
 
   return (
     <Modal
+      className={s.Modal}
       onRequestClose={closeModal}
       style={customStyles}
       isOpen={modalIsOpen}
     >
-      <img src={urls.regular} alt={alt_description} />
-      <button onClick={closeModal}>closeModal</button>
+      <img className={s.ImageModal} src={urls.regular} alt={alt_description} />
+      <button className={s.button_modalclose} onClick={closeModal}>
+        <IoCloseCircleOutline />
+      </button>
+      <div className={s.Items}>
+        {alt_description && (
+          <p className={s.modal_description}>Опиc: {alt_description}</p>
+        )}
+        {image.user && (
+          <p className={s.ImageCard_author}>Автор: {image.user.name}</p>
+        )}
+        <p className={s.modal_likes}>Лайки: {image.likes}</p>
+
+        <p className={s.modal_created}>Створено: {image.created_at}</p>
+        <p className={s.modal_downloads}>Завантажено: {image.downloads}</p>
+      </div>
     </Modal>
   );
 };
